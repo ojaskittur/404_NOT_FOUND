51c91a8a9c6e4e73599ce41a5e586b06bcf32708
ojaskittur <110086648+ojaskittur@users.noreply.github.com>
2023-09-20 14:26:56 +0530

    Update forms.py

--- django-upload-example/mysite/core/forms.py
+++ django-upload-example/mysite/core/forms.py
@@ -1,9 +1,25 @@
 from django import forms
-
 from .models import Book
 
+class ClearableTextInput(forms.TextInput):
+    def render(self, name, value, attrs=None, renderer=None):
+        # Set the default value to "No slot" if the value is empty
+        if not value:
+            value = 'No slot'
+        return super().render(name, value, attrs, renderer)
 
 class BookForm(forms.ModelForm):
+    slot = forms.CharField(
+        label='Slot',
+        required=False,
+        max_length=255,
+        widget=ClearableTextInput(attrs={'onfocus': 'clearDefaultValue(this)', 'onblur': 'restoreDefaultValue(this)'}),
+    )
+
     class Meta:
         model = Book
-        fields = ('title', 'author', 'pdf', 'cover')
+        fields = ('title', 'author', 'pdf')
+        labels = {
+            'title': 'Subject',
+            'author': 'Teacher',
+        }
